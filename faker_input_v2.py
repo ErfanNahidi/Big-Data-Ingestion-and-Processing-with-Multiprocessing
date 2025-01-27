ef9da1af5cd82be5167421bfceb1b1352ea2b5b6
Erfan Nahidi <119894834+ErfanNahidi@users.noreply.github.com>
2025-01-27 14:54:22 +0330

    Update faker_input_v2.py

--- faker_input_v2.py
+++ faker_input_v2.py
@@ -1,25 +1,3 @@
-'''from faker import Faker
-from random import randint
-
-class StreamFastGenerator:
-    def __init__(self, locale='fa_IR'):
-        self.faker = Faker(locale)
-    
-    def generate_data(self):
-        while True:
-            yield {
-                'first_name': self.faker.first_name(),
-                'last_name': self.faker.last_name(),
-                'age': randint(18, 100),
-                'state': self.faker.state(),
-                'city': self.faker.city(),
-                'street': self.faker.street_name(),
-                'building_number': self.faker.building_number(),
-                'phone_number': self.faker.phone_number(),
-                'post_code': self.faker.postcode()
-            }'''
-
-
 from faker import Faker
 from random import randint
 
